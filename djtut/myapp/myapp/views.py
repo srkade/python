d40655a013ea2095cdbaa6a271eeb5e2b90ec92f
srkade <shrikrushnade766@gmail.com>
2024-09-10 10:17:35 +0530

    updating transfering data from python to html

--- djtut/myapp/myapp/views.py
+++ djtut/myapp/myapp/views.py
@@ -2,8 +2,33 @@ from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
 def home(request):
+    # check=request.GET['check']
+
     date=datetime.datetime.now()
-    return render(request,"home.html",{})
+    isActive=True
+    name="learnCodeWithShree"
+    list_of_programs=[
+        'write a prgram to check even number',
+        'write a program to check prime numebr',
+        'write a prgram to pring all prime number from 1 to 100',
+        'write a program to print pascals triange'
+    ]
+    student ={
+        'name':"Shreee",
+        'last_name':"Ade",
+        'age':26,
+        'education':"Bachelor Of Engineering",
+        'address':"Chandan Nagar Pune 411014"
+
+    }
+    data={
+        'date':date,
+        'isActive':isActive,
+        'name':name,
+        'list_of_programs':list_of_programs,
+        'student_data':student,
+    }
+    return render(request,"home.html",data)
     # return HttpResponse("<h1>Hello this is index page </h1>"+str(date))
 
 def about(request):
